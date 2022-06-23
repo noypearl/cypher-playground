2abf79cdf7542f83f066e03a98f66d5ef69a1993
noypearl <11259340+noypearl@users.noreply.github.com>
2022-06-23 06:24:38 +0300

    fixed injection request url encode in swagger

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,4 +23,6 @@ cypher-shell -u $NEO4J_USER -p $NEO4J_PASS "CREATE DATABASE spongebob IF NOT EXI
 
 sleep 5
 echo neo4j - spongebob database is ready!
+echo Done! Server is up at http://localhost:3030
+echo try to access neo4j browser locally at http://0.0.0.0:7474 or see the swagger at http://localhost:8888
 tail -f /dev/null
